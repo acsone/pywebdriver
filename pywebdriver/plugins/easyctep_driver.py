553429264426d94affbf5c29914c2b3839b5c288
Denis Roussel <denis.roussel@acsone.eu>
2019-10-10 16:05:27 +0200

    [EASYCTEP] Improve code
    * Manages better terminal connection/disconnection
    * Uses more generic PaymentTerminal driver class
    * Adds the ticket printing support

--- pywebdriver/plugins/easyctep_driver.py
+++ pywebdriver/plugins/easyctep_driver.py
@@ -29,19 +29,23 @@ from flask import request, jsonify, render_template
 
 from pywebdriver import app, config, drivers
 
-from .base_driver import ThreadDriver
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
+from .payment_base_driver import PaymentTerminalDriver
 from easyctep import (
     SaleResultListener,
     TerminalManager,
-    TerminalLookupError,
     Terminal,
     get_library_version
 )
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)6s %(thread)5s %(message)s"
+)
+
 logger = logging.getLogger(__name__)
+
 
 class LimitedDict(OrderedDict):
     """ A dictionary that only keeps the last few added keys
@@ -63,86 +67,147 @@ class CTEPSaleResultListener(SaleResultListener):
         self.result = None
 
     def on_sale_result_success(self, terminal_id, **kwargs):
+        """
+        Catch sale result success and pass it to driver callback
+        :param terminal_id:
+        :param kwargs:
+        :return:
+        """
         self.result = True
-        self._callback(self.result, **kwargs)
-        logger.warning(
-            "on_sale_result_success on terminal %s: %s", terminal_id, kwargs
+        self._callback(terminal_id, self.result, **kwargs)
+        logger.info(
+            "Sale result succeeded on terminal %s: %s", terminal_id, kwargs
         )
 
     def on_sale_result_error(self, terminal_id, **kwargs):
+        """
+        Catch sale result error and pass it to driver callback
+        :param terminal_id:
+        :param kwargs:
+        :return:
+        """
         self.result = False
-        self._callback(self.result,** kwargs)
-        logger.warning("on_sale_result_error on terminal %s: %s", terminal_id, kwargs)
+        self._callback(terminal_id, self.result, ** kwargs)
+        logger.warning(
+            "Sale result error on terminal %s: %s", terminal_id, kwargs
+        )
 
     def wait_for_result(self, callback):
+        """
+        Will associate the callback function to sale result listener
+        :param callback:
+        :return:
+        """
         self._callback = callback
         while self.result is None:
             time.sleep(1)
 
 
+class CTEPTerminalManager(TerminalManager):
+
+    def on_terminal_connect(self, terminal):
+        """
+        Will catch terminal connections and send result to driver
+        :param terminal: Terminal()
+        :return:
+        """
+        res = super(CTEPTerminalManager, self).on_terminal_connect(terminal)
+        self._callback(terminal.terminal_id, connected=True)
+        return res
+
+    def on_terminal_disconnect(self, terminal_id):
+        """
+                Will catch terminal disconnections and send result to driver
+                :param terminal_id: byte
+                :return:
+                """
+        res = super(CTEPTerminalManager, self).on_terminal_disconnect(
+            terminal_id)
+        self._callback(terminal_id, connected=False)
+        return res
+
+    def register_ctep_callback(self, callback):
+        """
+        Register Ctep driver callback function
+        :param callback:
+        :return:
+        """
+        self._callback = callback
+
+
 ctep_sale_result_listener = CTEPSaleResultListener()
 
 
-class CTEPDriver(ThreadDriver):
+class CTEPDriver(PaymentTerminalDriver):
 
     def __init__(self, cfg, *a, **kw):
-        super(CTEPDriver, self).__init__(*a, **kw)
-        self._init_driver(config=cfg)
+        super(CTEPDriver, self).__init__(cfg, *a, **kw)
+        self._init_driver()
         
-    def _init_driver(self, config):
+    def _init_driver(self):
         """
         Initialize CTEP driver
         """
         logger.info("%s - %s " % (self, "CTEP Initialization"))
-        logger.info("%s - %s " % (self, " - ".join(["CTEP Library version", get_library_version()])))
+        logger.info(
+            "%s - %s " %
+            (self, " - ".join(["CTEP Library version", get_library_version()]))
+        )
         # Specific to match 'sale identifier' with Sale Order number
-        # Each entry is composed by <salesystemIdentifier> : {'order_id': <order reference>, 'amount': <amount>}
+        # Each entry is composed by <salesystemIdentifier> :
+        # {'order_id': <order reference>, 'amount': <amount>}
         self.current_transactions = {}
-        # Other properties
-        self.cfg = config
-        self.service_port = self.cfg.get('service_port', 9000)
-        self.terminal = False
-        self.in_transaction = False
-        self.transactions_count = 0
-        self.transactions_cache = LimitedDict()
-        self.service = TerminalManager.make_tcp_ip_service(self.service_port)
-        if self.cfg.get('certification_mode', False):
-            self.service.set_certification_mode(os.path.join(os.path.dirname(__file__), "easyctep_service.log"))
+        ctep_sale_result_listener._callback = self.set_result
 
-    def run(self):
-        """
-        Launched when Application start
-        """
+        self.service_port = self.config.get('service_port', 9000)
+        self.server_mode = True\
+            if self.config.get('server_mode', False) == 'True' else False
+        self.print_receipt = True\
+            if self.config.get('print_receipt', False) == 'True' else False
+        self.service = CTEPTerminalManager.create_tcp_ip(self.service_port)
+        if self.config.get('certification_mode', False):
+            self.service.set_certification_mode(
+                os.path.join(
+                    os.path.dirname(__file__), "easyctep_service.log"))
+
+        # run TerminalManager service
         self.service.start()
         # Wait asynchronously for terminal
         self.push_task("_wait_for_terminal")
-        super(CTEPDriver, self).run()
+
+    def _set_terminal_status(self, terminal_id, connected):
+        """
+        This is intended to be passed as callback to TerminalManager
+        It is used to update the terminal state in the driver
+        :param terminal:
+        :param connected:
+        :return:
+        """
+        state = 'connected' if connected else 'disconnected'
+        if state == 'connected':
+            terminal = self.service.terminal_by_id(terminal_id)
+            # Get last transaction status
+            # It will go into the callback to set transaction status
+            # if this was raised after a terminal disconnection
+            terminal.last_transaction_status(ctep_sale_result_listener)
+        self._set_state(terminal_id, state)
+        logger.info(
+            'CTEP : Terminal %s changed status to %s' % (terminal_id, state))
 
     def _wait_for_terminal(self, data):
-        while True:
-            try:
-                self.terminal = self.service.current_terminal()
-                return
-            except TerminalLookupError:
-                time.sleep(1)
-
-    def get_status(self):
-        """
-        Return Terminal status
-        """
-        status = {
-            'status': 'connected' if self.terminal else 'disconnected',
-            'in_transaction': self.in_transaction,
-            'transactions_count': self.transactions_count,
-            'latest_transactions': self.transactions_cache,
-        }
-        return status
+        CTEPTerminalManager.register_ctep_callback(
+            CTEPTerminalManager, self._set_terminal_status)
 
     def get_vendor_product(self):
         return 'easyctep'
  
     def get_payment_info_from_price(self, price, payment_mode):
-        logger.info("Payment mode: %s", payment_mode)
+        """
+        This is used for the test interface
+        :param price:
+        :param payment_mode:
+        :return:
+        """
         return {
             'amount': price,
             'payment_mode': payment_mode,
@@ -151,56 +216,79 @@ class CTEPDriver(ThreadDriver):
 
     def _wait_for_sale_result(self, data):
         """
-        Wait for sale transaction result
+        Wait for sale transaction result and pass the callback
         """
         ctep_sale_result_listener.wait_for_result(self.set_result)
 
-    def transaction_start(self, info):
-        self.in_transaction = True
-        self.transactions_count += 1
-        info = json.loads(info)
-        order_id = info['order_id']
-        if not self.terminal:
-            logger.warn("Terminal not registered yet, can't start transaction")
+    def _transaction_start(self, terminal_id, payment_info):
+        """
+        Start a CTEP transaction:
+            * Build a new sale unique identifier
+            * Send the transaction to the registered terminal
+            * Wait for result (use of CTEPSaleResultListener for callbacks)
+        :param terminal_id: str
+        :param payment_info: dict
+        :return:
+        """
+        if terminal_id not in self.terminals or\
+                self.terminals[terminal_id]['state'] != 'connected':
+            logger.warning(
+                "CTEP : Terminal not registered yet, can't start transaction")
             return
-        logger.info("Starting transaction")
-        amount = info['amount']
+        order_id = payment_info['order_id']
+        amount = payment_info['amount']
         sale_identifier = self.service.new_sale_system_action_identifier()
         # TODO: Remove
         sale_identifier = int(sale_identifier)
-
+        # Add transaction to current ones (the cache)
         self.current_transactions[sale_identifier] = {
             'order_id': order_id,
             'amount': amount,
         }
-        self.terminal.send_sale_transaction(
+        logger.info("CTEP : Starting transaction")
+        self.service._terminals[terminal_id].send_sale_transaction(
             amount, order_id, sale_identifier, ctep_sale_result_listener
         )
         self.push_task("_wait_for_sale_result")
     
-    def set_result(self, result, **kwargs):
+    def set_result(self, terminal_id, result, **kwargs):
         """
         Callback method to be called by SaleListener
+        Get the transaction concerned by the trigger and if result == True,
+        add a line to transactions_cache.
         """
-        self.in_transaction = False
+        self.terminals[terminal_id]['in_transaction'] = False
         if not result:
             return
         sale_identifier = kwargs.get("sale_system_action_identifier")
-        if sale_identifier:
+        if sale_identifier and sale_identifier in self.current_transactions:
             sale = self.current_transactions[sale_identifier]
-            self.transactions_cache.setdefault(sale.get("order_id"), []).append({
-                        'amount_cents': kwargs.get("authorized_amount"),
-                    })
+            order_id = sale.get("order_id")
+            authorized_amount = kwargs.get("authorized_amount")
+            self._add_transaction(order_id, authorized_amount, terminal_id)
+            ticket = kwargs.get('client_ticket')
+            if self.print_receipt and ticket:
+                terminal = self.service.terminal_by_id(terminal_id)
+                terminal.send_print_ticket_transaction(ticket)
+
 
 def load_driver_config():
+    """
+    Utility function to load correctly configuration options
+    :return:
+    """
     driver_config = {}
-    # Put here mandatory config entiries
+    default_keys = (
+        ('print_receipt', 'yes'),
+        ('server_mode', False),
+        ('certification_mode', False))
+    # Put here mandatory config entries
     for key in ():
         try:
             driver_config[key] = config.get('easyctep_driver', key)
         except NoOptionError:
             raise Exception("Missing configuration for ctep driver: %s" % key)
-    for key, default in (('print_receipt', 'yes'),):
+    for key, default in default_keys:
         if config.has_option('easyctep_driver', key):
             driver_config[key] = config.get('easyctep_driver', key)
         else:
@@ -210,7 +298,8 @@ def load_driver_config():
 
 easyctep_driver = CTEPDriver(load_driver_config())
 drivers['easyctep'] = easyctep_driver
-easyctep_driver.start()
+if not easyctep_driver.is_alive():
+    easyctep_driver.start()
 
 
 @app.route(
